71ff3a5fbea5a1f22e1f28f83b20048c9f4deafa
Deyna <d.baeva@student.fontys.nl>
2020-10-09 11:35:56 +0200

    fetching real data from api

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Line, Bar } from 'react-chartjs-2';
+
+import { fetchDailyData } from '../../api';
+
+import styles from './Chart.module.css';
 
 const Chart = () => {
+    // once we fetch the data, we'll set it to the state and provide the initial value, which is an empty object
+    const [dailyData, setDailyData] = useState({})
+
+    useEffect(() => {
+        const fetchAPI = async() =>{
+            setDailyData(await fetchDailyData());
+        }
+
+        fetchAPI();
+    });
+
+    const lineChart = (
+        <Line 
+            data={{
+                labels: '',
+                datasets: [{}, {}],
+            }}
+        />
+    );
+
     return(
         <h1>Chart</h1>
     )
